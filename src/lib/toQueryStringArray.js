dcabdbea0df026a867814c23582ddd239c159c40
Peter Murphy <peter.francis.murphy@gmail.com>
2019-07-08 10:48:22 -0400

    Implement toQueryStringArray, add tests

--- src/lib/toQueryStringArray.js
+++ src/lib/toQueryStringArray.js
@@ -1,25 +1,18 @@
 import { unwords } from ".";
+import normalize from "./normalize";
+import subsequences from "./subsequences";
 
-export const toQueryString = ({ keywords, labels, language }) =>
-  `${keywords} ${unwords(
-    labels.map(label => `label:"${label}"`)
-  )} language:${language}`;
+export const toQueryString = ({ keywords = "", labels, language }) =>
+  unwords([
+    keywords,
+    unwords(labels.map(label => `label:"${label}"`)),
+    language === "Any" ? "" : `language:${language}`
+  ]);
 
-// @TODO: Implement `toQueryStringArray` function
-// Combinations of labels in a query will only
-// yield results where _all_ labels are present.
-// We want to be a bit less strict, so if a user
-// enters multiple, we want to generate the power
-// set of possible combinations, and distribute
-// across multiple queries. An example: a user
-// searches for label "a", label "b", and label "c".
-// We want to yield results for when all three
-// labels appear together, _then_ we want results
-// where just "a" and "b" are present, then match
-// "a" and "c", then "b" and "c", then match each
-// label individually.
-// I've implemented a `subsequences` function to
-// help with this.
-export default ({ keywords, labels, language }) => [
-  toQueryString({ keywords, labels, language })
-];
+export default ({ keywords, labels, language }) =>
+  (labels.length > 0
+    ? subsequences(labels).map(ls =>
+        toQueryString({ keywords, labels: ls, language })
+      )
+    : [toQueryString({ keywords, labels, language })]
+  ).map(normalize);
--- src/lib/toQueryStringArray.test.js
+++ src/lib/toQueryStringArray.test.js
@@ -1,14 +1,52 @@
 import toQueryStringArray from "./toQueryStringArray";
 
-describe("toQueryString", () => {
+describe("toQueryStringArray", () => {
   it("constructs a valid query given non-empty keywords, labels, and language", () => {
     const input = {
       keywords: "test",
       labels: ["good first issue", "help wanted"],
       language: "JavaScript"
     };
+    expect(toQueryStringArray(input)).toHaveLength(3);
     expect(toQueryStringArray(input)[0]).toBe(
       'test label:"good first issue" label:"help wanted" language:JavaScript'
+    );
+    expect(toQueryStringArray(input)[1]).toBe(
+      'test label:"good first issue" language:JavaScript'
+    );
+    expect(toQueryStringArray(input)[2]).toBe(
+      'test label:"help wanted" language:JavaScript'
+    );
+  });
+  it("constructs a valid query given empty labels array", () => {
+    const input = {
+      keywords: "example search",
+      labels: [],
+      language: "JavaScript"
+    };
+    expect(toQueryStringArray(input)).toHaveLength(1);
+    expect(toQueryStringArray(input)[0]).toBe(
+      "example search  language:JavaScript"
+    );
+  });
+  it("constructs a valid query given empty search and empty labels array", () => {
+    const input = {
+      keywords: "",
+      labels: [],
+      language: "JavaScript"
+    };
+    expect(toQueryStringArray(input)).toHaveLength(1);
+    expect(toQueryStringArray(input)[0]).toBe("language:JavaScript");
+  });
+  it('constructs a valid query given "Any" as language', () => {
+    const input = {
+      keywords: "example",
+      labels: ["help wanted", "good first issue"],
+      language: "Any"
+    };
+    expect(toQueryStringArray(input)).toHaveLength(3);
+    expect(toQueryStringArray(input)[0]).toBe(
+      'example label:"help wanted" label:"good first issue"'
     );
   });
 });
