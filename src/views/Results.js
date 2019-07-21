7f185d63fc5265f8da94aacf0d2aa09662e052a9
Peter Murphy <peter.francis.murphy@gmail.com>
2019-07-21 16:02:30 -0400

    :sparkles: POC for Results display
    This is very rough, just showing the raw JSON more-or-less. Haven't polished the GQL query, and the variables are hardcoded in. Also, to work on this locally, you'll need a .env file with a REACT_APP_GH_KEY variable for hitting the GitHub API. I'll post instructions on how to get one set up when I can.

--- src/views/Results.js
+++ src/views/Results.js
@@ -1,12 +1,78 @@
 import React from "react";
+import ApolloClient, { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import get from "lodash/fp/get";
+import compose from "lodash/fp/compose";
+import map from "lodash/fp/map";
+
+// @TODO: Add REACT_APP_GH_KEY to Now variables
+const headers = {
+  authorization: `Bearer ${process.env.REACT_APP_GH_KEY}`
+};
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers
+});
+
+// This lens will drill down into the returned JSON to get
+// the actual relevant data that we want. Feel free to experiment
+// with it, or we can remove it altogether. Lenses add safety for
+// getting deeply nested data out of JSON but with how Apollo works,
+// we might not need it.
+const exampleLens = compose(
+  map(get(["node"])),
+  get(["search", "edges"])
+);
 
 function Results() {
+  const { data, loading } = useQuery(exampleQuery, {
+    client,
+    // @TODO: Read variables from URL query string
+    // These variables are hardcoded right now, but ultimately we'll
+    // need to read them from the query string:
+    // https://en.wikipedia.org/wiki/Query_string
+    variables: {
+      query: 'react label:"good first issue"'
+    }
+  });
   return (
     <main>
       <h1>GitHub Issue Finder</h1>
       <h2>Results</h2>
+      <pre>
+        {/* @TODO: Replace this `stringify` call with a component for each issue */}
+        {loading ? "Loading" : JSON.stringify(exampleLens(data), null, 2)}
+      </pre>
     </main>
   );
 }
 
 export default Results;
+
+// @TODO: Fix this query to get all the info that is illustrated in the mock-up
+const exampleQuery = gql`
+  query ExampleQuery($query: String!) {
+    search(query: $query, first: 10, type: ISSUE) {
+      edges {
+        node {
+          ... on Issue {
+            repository {
+              owner {
+                login
+              }
+              updatedAt
+            }
+            updatedAt
+            title
+            labels(first: 10) {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
